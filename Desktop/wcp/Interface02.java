a436744819924cc95396465af9ab01feeffd54b3
Noi Fujii <noinoi51267@gmail.com>
2022-02-10 23:07:42 +0900

    [Add]Javaのdefault

---
+++ Desktop/wcp/Interface02.java
@@ -0,0 +1,13 @@
+package extend;
+
+import extend.phone.MobilePhone;
+
+public class Interface02 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		MobilePhone mobilePhone = new MobilePhone("000-1111-2222");
+		mobilePhone.powerOff();
+	}
+
+}
