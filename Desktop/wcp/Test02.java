187d04d0fe284571dcbb7d4eef5b856f32eec1ef
Noi Fujii <noinoi51267@gmail.com>
2022-02-13 00:04:43 +0900

    [Add]Java学習

--- Desktop/wcp/Test02.java
+++ Desktop/wcp/Test02.java
@@ -1,23 +1,36 @@
 package test;
 
-import java.util.Arrays;
-import java.util.StringJoiner;
+import java.util.ArrayList;
 public class Test02 {
-
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		int num[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
-		int sum = 0;
-		StringJoiner sj = new StringJoiner(",");
-		Arrays.stream(num).forEach(i -> sj.add(String.valueOf(i)));
-		System.out.println(sj.toString());
-//		System.out.println(num[0]);
-		for (int i = 0; i < 20; ++i) {
-//			System.out.println(num[i]);
-			sum = num[i] + 0 + 1 + 2 + 3 + 4 + 5;
-			if (sum % 2 != 0) {
-				System.out.println(sum);
-			} 
+		int num[] = new int[19];  //配列の変数宣言
+		ArrayList<Integer> odds = new ArrayList<>(); //奇数
+		ArrayList<Integer> evens = new ArrayList<>(); //偶数
+		
+		for (int i = 0; i <= num.length - 1; i++) {
+			if (i == 0) {
+				num[i] = 0;
+				continue;
+			}
+			
+			num[i] = num[i - 1] + 5;
+			
+			if (i <= 10) {
+				if (num[i] % 2 != 0) {
+					odds.add(num[i]);
+				}
+			} else {
+				if (num[i] % 2 == 0) {
+					evens.add(num[i]);
+				}
+			}
+//			System.out.println(num[i - 1]);
+
 		}
+//		StringJoiner all = new StringJoiner(" ").merge(odds).merge(evens);
+//		System.out.println(all.toString());
+		
+		System.out.println(odds);
+		System.out.println(evens);
 	}
 }
